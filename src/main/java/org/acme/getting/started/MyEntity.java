5416302b475093e014b55e2c5c2521b56aa7cf62
Karmen Bezlaj <kbezlaj@cataneo.de>
2023-12-29 17:34:30 +0100

    Rest server structure done

---
+++ src/main/java/org/acme/getting/started/MyEntity.java
@@ -0,0 +1,13 @@
+package org.acme.getting.started;
+
+public class MyEntity {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
